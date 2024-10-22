a06f9a4fb9c650a29f931ba8160aac263bf6e19c
Hubert Leterme <hubert.leterme@unicaen.fr>
2024-10-22 19:29:17 +0200

    New Python script to compute KS or Wiener estimate and save it into the HDF5 file

---
+++ get_inputs_for_ssl.py
@@ -0,0 +1,185 @@
+import argparse
+import random
+import h5py
+import numpy as np
+
+import wlmmuq.kappatng as wlktng
+import wlmmuq.cosmos as wlcosmos
+import wlmmuq.utils as wlutils
+
+INPUT_WLMETHOD = "wiener"
+FWHM = 2.4 # As in Starck et al. (2021) (Gaussian smoothing for KS)
+NIMGS = 70816
+IMGSIZE = 306
+BATCH_SIZE = 32
+NIMGS_PS = 256
+
+def main(
+        path_to_augmented_dataset, input_wlmethod=INPUT_WLMETHOD,
+        fwhm=FWHM, nimgs=NIMGS, imgsize=IMGSIZE, batch_size=BATCH_SIZE,
+        seed=None, verbose=False, **kwargs
+):
+    if seed is not None:
+        random.seed(seed)
+        np.random.seed(seed)
+
+    # Compute a map of number of galaxies per pixels and a binary mask
+    if verbose:
+        print("Compute a map of number of galaxies per pixels and a binary mask")
+    cat_cosmos_bright, _ = wlcosmos.cosmos_catalog()
+    cat_cosmos_bright = cat_cosmos_bright[
+        cat_cosmos_bright['zphot'] >= np.min(wlktng.LIST_OF_Z)
+    ]
+    cat_cosmos_bright = cat_cosmos_bright[
+        cat_cosmos_bright['zphot'] < np.max(wlktng.LIST_OF_Z)
+    ]
+
+    openingangle = wlktng.get_openingangle(imgsize)
+    data_cosmos = wlcosmos.get_data_from_cosmos(
+        cat_cosmos_bright, openingangle
+    )
+    extent = data_cosmos['extent']
+    ngal = wlutils.ngal_per_pixel(
+        cat_cosmos_bright['Ra'], cat_cosmos_bright['Dec'],
+        imgsize, extent
+    )
+    mask = ngal > 0
+
+    shapedisp1, shapedisp2 = data_cosmos["shapedisp"]
+    shapedisp = (shapedisp1 + shapedisp2) / 2
+
+    # Compute noise covariance matrix
+    if verbose:
+        print("Compute noise covariance matrix")
+    std_noise = wlutils.get_std_noise(ngal, shapedisp, std_noise_mask=0)
+
+    # Initialize batch generators for training and validation
+    if input_wlmethod == 'ks':
+        if fwhm is not None:
+            resolution = openingangle / imgsize * 60. # arcmin/pixel
+            std_gaussianfilter_arcmin = fwhm / (2 * np.sqrt(2 * np.log(2)))
+            std_gaussianfilter = std_gaussianfilter_arcmin / resolution # pixels
+            kwargs.update(std_gaussianfilter=std_gaussianfilter)
+
+    elif input_wlmethod == 'wiener':
+        if verbose:
+            print("Estimate the power spectrum for Wiener filtering")
+        # Load a set of convergence maps among the training set
+        train_gen_ps = wlutils.HDF5BatchLoader(
+            path_to_augmented_dataset, nimgs=NIMGS_PS, batch_size=NIMGS_PS,
+            std_noise=std_noise, mask=mask, output_shape=imgsize,
+            list_of_outputs=['kappa_true']
+        )
+        kappa_ps = train_gen_ps.load_batch()
+        train_gen_ps.close()
+
+        # Compute the 1D power spectrum
+        powerspectrum_1d = wlutils.get_1d_powerspectrum(kappa_ps)
+        del kappa_ps
+        kwargs.update(powerspectrum_1d=powerspectrum_1d)
+
+    else:
+        raise ValueError
+
+    if verbose:
+        print("Initialize batch generator")
+    data_loader = wlutils.HDF5BatchLoader(
+        path_to_augmented_dataset, nimgs=nimgs, batch_size=batch_size,
+        std_noise=std_noise, mask=mask, shuffle=False, output_shape=imgsize,
+        list_of_outputs=['kappa_inp'],
+        input_method=input_wlmethod, close_after_batch=True, **kwargs
+    )
+    data_gen = data_loader.to_tf_dataset(raise_stop_iteration=True)
+    data_gen = iter(data_gen)
+
+    with h5py.File(path_to_augmented_dataset, 'r+') as file:
+        idx_dataset = f"kappa_{input_wlmethod}"
+        try:
+            del file[idx_dataset]
+        except KeyError:
+            pass
+        else:
+            if verbose:
+                print(
+                    f"Found existing dataset for {idx_dataset}; "
+                    "it will be overwritten."
+                )
+        file.create_dataset(
+            idx_dataset, shape=(nimgs, imgsize, imgsize),
+            dtype='float32'
+        )
+        end_idx = 0
+        while True:
+            beg_idx = end_idx
+            try:
+                kappa_inp = next(data_gen)
+            except StopIteration:
+                break
+
+            end_idx = beg_idx + kappa_inp.shape[0]
+            if verbose:
+                print(f"Processing images {beg_idx} to {end_idx}")
+            file[idx_dataset][beg_idx:end_idx] = kappa_inp
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "path_to_augmented_dataset", type=str,
+        help="Path to the augmented dataset (HDF5 file)"
+    )
+    parser.add_argument(
+        "--input-wlmethod", type=str,
+        default=argparse.SUPPRESS,
+        help=(
+            "Weak lensing method used as input ('wiener' or 'ks'). "
+            f"Default = '{INPUT_WLMETHOD}'"
+        )
+    )
+    parser.add_argument(
+        "--fwhm", type=int,
+        default=argparse.SUPPRESS,
+        help=(
+            "If the selected method is Kaiser-Squires ('ks'), FWHM of "
+            f"the smoothing filter, in arcmin. Default = {FWHM}"
+        )
+    )
+    parser.add_argument(
+        "--nimgs", type=int,
+        default=argparse.SUPPRESS,
+        help=(
+            "Number of images in the training set. "
+            f"Default = {NIMGS}"
+        )
+    )
+    parser.add_argument(
+        "--imgsize", type=int,
+        default=argparse.SUPPRESS,
+        help=(
+            "Number of pixels (width) in input images. "
+            f"Default = {IMGSIZE}"
+        )
+    )
+    parser.add_argument(
+        "-b", "--batch-size", type=int,
+        default=argparse.SUPPRESS,
+        help=(
+            "Batch size for training and validation. "
+            f"Default = {BATCH_SIZE}"
+        )
+    )
+    parser.add_argument(
+        "--seed", type=int,
+        default=argparse.SUPPRESS,
+        help=(
+            "Seed for the random number generators"
+        )
+    )
+    parser.add_argument(
+        "-v", "--verbose", action='store_true',
+        default=argparse.SUPPRESS
+    )
+
+    args = parser.parse_args()
+    kwargs = vars(args).copy()
+    main(**kwargs)
