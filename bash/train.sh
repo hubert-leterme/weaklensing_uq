884b14206fce240c275584ad0d5c236d2a80cf9a
Hubert Leterme <leterme231@N315L-G17G01.ressource.unicaen.fr>
2024-10-22 17:39:47 +0200

    Bash script for training

---
+++ bash/train.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Set paths
+path_to_augmented_dataset=/ceph/chercheurs/leterme231/kappaTNG_augmented/LP002_augmented.hdf5
+checkpoint_dir=/ceph/chercheurs/leterme231/checkpoints
+save_freq=8
+backup_dir=/ceph/chercheurs/leterme231/backups
+
+# Check if correct number of arguments are provided
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <GPU_ID> <WL_METHOD>"
+  echo "Example: $0 0 ks"
+  exit 1
+fi
+
+# Set environment variables and run the task
+export CUDA_VISIBLE_DEVICES=$1
+python run_deepmass.py $path_to_augmented_dataset \
+  --input-wlmethod $2 \
+  --checkpoint-dir $checkpoint_dir/checkpoint_$2 \
+  --save-freq $save_freq \
+  --backup-dir $backup_dir/backup_$2 \
+  -log log_$2.csv --seed 42 -v
+  #--nimgs-train 512 --nimgs-val 128 --nepochs 2
