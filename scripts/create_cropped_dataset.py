b6b316873ca6d9146a082e860521b0a4888347a8
Hubert Leterme <hubert.leterme@unicaen.fr>
2024-10-25 14:16:56 +0000

    New Python script

---
+++ scripts/create_cropped_dataset.py
@@ -0,0 +1,67 @@
+import argparse
+
+import wlmmuq.kappatng as wlktng
+import wlmmuq.cosmos as wlcosmos
+
+OPENINGANGLE = 1.5 # Opening angle
+NINPIMGS = 100 # Number of input images
+
+def main(
+        path_to_cropped_dataset, idx_lp=None,
+        openingangle=OPENINGANGLE, ninpimgs=NINPIMGS,
+        **kwargs
+):
+    # Get redshift weights from the COSMOS catalog
+    cat_cosmos_bright, _ = wlcosmos.cosmos_catalog()
+    cat_cosmos_bright = wlktng.filter_by_redshifts(cat_cosmos_bright)
+    weights_redshift = wlktng.get_weights(cat_cosmos_bright['zphot'])
+
+    # Get nb of pixels in output images and adjust opening angle accordingly
+    imgsize, openingangle = wlktng.get_npixels_openingangle(openingangle)
+
+    # Create augmented dataset and store data
+    wlktng.create_cropped_dataset(
+        path_to_cropped_dataset, idx_lp, ninpimgs, weights_redshift, imgsize,
+        **kwargs
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "path_to_cropped_dataset", type=str,
+        help="Path to the augmented dataset (HDF5 file)"
+    )
+    parser.add_argument(
+        "--idx-lp", type=str,
+        default=argparse.SUPPRESS,
+        help=(
+            "Index of the learning potential, indicating which folder to look "
+            "into for the HDF5 files containing the dataset (`LPxxx` where `xxx` "
+            "ranges from `001` to `100`). Default = `001`"
+        )
+    )
+    parser.add_argument(
+        "--openingangle", type=float,
+        default=argparse.SUPPRESS,
+        help=f"Opening angle (deg). Default = {OPENINGANGLE}"
+    )
+    parser.add_argument(
+        "--ninpimgs", type=int,
+        default=argparse.SUPPRESS,
+        help=(
+            f"Number of images to reconstruct. Default = {NINPIMGS}"
+        )
+    )
+    parser.add_argument(
+        "-b", "--batch-size", type=int,
+        default=argparse.SUPPRESS,
+        help=(
+            "Batch size, to avoid memory overload. "
+            "Default = 50"
+        )
+    )
+
+    args = parser.parse_args()
+    kwargs = vars(args).copy()
+    main(**kwargs)
