1928483ee9330729796335379470bb6c4113c049
Hubert Leterme <hubert.leterme@unicaen.fr>
2024-11-13 12:54:38 +0100

    Housekeeping

--- wlmmuq/cqr.py
+++ wlmmuq/cqr.py
@@ -74,7 +74,7 @@ class BaseCQR:
         res_cqr_test = self._calibration_fun(quantile_vals, res_test)
 
         return res_cqr_test, quantile_vals, adjusted_quantile
-    
+
 
     def get_bounds_proba(self, nimgs_calib):
         lower_bound_proba = self.alpha - 1 / (nimgs_calib + 1)
