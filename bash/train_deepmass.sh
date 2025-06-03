bbccf494c9d971e749a1f6b0a456003c80887a87
Hubert Leterme <hubert.leterme@unicaen.fr>
2025-06-03 16:40:49 +0200

    Update bash scripts (denoiser and DeepMass)

--- bash/train_deepmass.sh
+++ bash/train_deepmass.sh
@@ -8,16 +8,28 @@ save_freq=1
 current_date=$(date +"%Y%m%d_%H%M%S")
 
 # Check if correct number of arguments are provided
-if [ "$#" -ne 3 ]; then
-  echo "Usage: $0 <GPU_ID> <WL_METHOD> <LEARNING_RATE>"
-  echo "Example: $0 0 ks 1e-4"
+if [ "$#" -lt 2 ]; then
+  echo "Usage: $0 <GPU_ID> <INPUT_METHOD> [OPTION1 [OPTION 2 ...]]"
+  echo "Example: $0 0 wiener [-m <filename> OR -a tensorflow.UNet] [--loss mse]"
   exit 1
 fi
 
+optional_args="${@:3}"
+
+# Set name of the denoiser
+optional_args_cleaned=$(echo "$optional_args" | sed 's/-m //g' | sed 's/-a //g' | sed 's/-s /--model-size /g' | sed 's/-p /--pretrained /g' | sed 's/-w [0-9]\+//g' | sed 's/-b /--batch-size /g' | sed 's/-e /--nepochs /g' | sed 's/-lr /--learning-rate /g' | sed 's/--//g' | xargs | sed 's/ /_/g' | sed 's/\.keras//g')
+name_denoiser=$(echo "denoiser_${optional_args_cleaned}_${current_date}" | sed 's/__/_/g')
+
+# Command to execute
+cmd=$(echo "python scripts/train.py ${path_to_augmented_dataset} --input-method $2 ${optional_args} --lr-scheduler --checkpoint-dir ${checkpoint_dir}/${name_denoiser} --save-freq ${save_freq} --backup-dir ${backup_dir}/${name_denoiser} --path-to-csv-log ${stats_dir}/log_${name_denoiser}_pe.csv --seed 42 -v" | xargs)
+
+# Print the command for tracking
+echo "Running the following command:"
+echo "=============================================================================="
+echo "$cmd"
+echo "=============================================================================="
+echo ""
+
 # Set environment variables and run the task
 export CUDA_VISIBLE_DEVICES=$1
-python scripts/train.py $path_to_augmented_dataset \
-  --input-method $2 \
-  -lr $3 --lr-scheduler \
-  --checkpoint-dir $checkpoint_dir/checkpoint_${2}_${current_date} \
-  --seed 42 -v
+eval "$cmd"
--- bash/train_denoiser.sh
+++ bash/train_denoiser.sh
@@ -2,7 +2,6 @@
 
 # Set paths
 path_to_augmented_dataset=/ceph/chercheurs/leterme231/kappaTNG_augmented/LP002_augmented_384.hdf5
-path_to_models=/ceph/chercheurs/leterme231/models
 checkpoint_dir=/ceph/chercheurs/leterme231/checkpoints
 save_freq=1
 
@@ -20,14 +19,6 @@ optional_args="${@:2}"
 # Set name of the denoiser
 optional_args_cleaned=$(echo "$optional_args" | sed 's/-m //g' | sed 's/-a //g' | sed 's/-s /--model-size /g' | sed 's/-p /--pretrained /g' | sed 's/-w [0-9]\+//g' | sed 's/-b /--batch-size /g' | sed 's/-e /--nepochs /g' | sed 's/-lr /--learning-rate /g' | sed 's/--//g' | xargs | sed 's/ /_/g')
 name_denoiser=$(echo "denoiser_${optional_args_cleaned}_${current_date}" | sed 's/__/_/g')
-
-# Check if argument `-m <model_filename>` is provided and
-# update optional arguments with full path to model
-if [[ $optional_args == *"-m "* ]]; then
-  model_filename=$(echo "$optional_args" | xargs | grep -oP '\-m \K[^\s]+' | xargs)
-  optional_args=$(echo "$optional_args" | sed "s/-m $model_filename//g" | xargs)
-  optional_args="-m ${path_to_models}/${model_filename} ${optional_args}"
-fi
 
 # Command to execute
 cmd=$(echo "python scripts/train.py ${path_to_augmented_dataset} --denoiser ${optional_args} --lr-scheduler --checkpoint-dir ${checkpoint_dir}/${name_denoiser} --seed 42 -v" | xargs)
