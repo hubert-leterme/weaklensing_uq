252c74cfdd05024a42328568b7a5a723b6520221
Hubert Leterme <hubert.leterme@unicaen.fr>
2025-06-05 16:28:40 +0200

    Update `MODEL_CLASSES`

--- wlmmuq/models/__init__.py
+++ wlmmuq/models/__init__.py
@@ -7,6 +7,7 @@ if USE_TENSORFLOW:
 MODEL_CLASSES = {
     "torch.UNet": (torch.UNet, False),
     "torch.UNetWienerInit": (torch.UNetWienerInit, False),
+    "torch.SUNetWienerInit": (torch.SUNetWienerInit, False),
     "torch.DRUNet": (torch.DRUNet, True),
     "torch.SUNet": (torch.SUNet, False),
     "torch.SUNetNoiseAware": (torch.SUNetNoiseAware, True),
