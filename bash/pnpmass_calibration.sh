c9fabef5b638cb9cf3d61fccff545947760fd68f
Hubert Leterme <hubert.leterme@unicaen.fr>
2025-08-19 15:31:24 +0200

    Update bash scripts

--- bash/pnpmass_calibration.sh
+++ bash/pnpmass_calibration.sh
@@ -20,6 +20,7 @@ mkdir -p ${checkpoint_dir}/cqr_pnpmass
 optional_args_cleaned=$(echo "$optional_args" \
   | sed 's|--checkpoint-dir-uq [^ ]\+|alternativemn|g' \
   | sed 's/--switch-mode-for-uq//g' \
+  | sed 's/--starlet -thresh /--starlet /g' \
   | sed 's/-auq [^ ]\+//g' \
   | sed 's/-suq [^ ]\+//g' \
   | sed 's/-a [^ ]\+//g' \
