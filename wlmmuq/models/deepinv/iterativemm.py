aeb41f78ee89562d363f9678e7eaa77a1fb977ce
Hubert Leterme <hubert.leterme@unicaen.fr>
2025-08-20 16:03:05 +0200

    Add mean centering to `ProximalWiener`

--- wlmmuq/models/deepinv/iterativemm.py
+++ wlmmuq/models/deepinv/iterativemm.py
@@ -134,9 +134,10 @@ class MassMapping(dinv.physics.LinearPhysics):
 
 class ProximalWiener(nn.Module):
 
-    def __init__(self, powerspectrum):
+    def __init__(self, powerspectrum, meancentering=True):
         super().__init__()
         self.register_buffer("powerspectrum", powerspectrum)
+        self.meancentering = meancentering
 
 
     def forward(
@@ -148,6 +149,8 @@ class ProximalWiener(nn.Module):
         out = torch.fft.fft2(inp)
         out /= (1 + g_param / self.powerspectrum)
         out = torch.fft.ifft2(out)
+        if self.meancentering:
+            out = utils.meancenter(out)
 
         return out.real
 
