899ccf81a2f74653144453bce8574403135fc15a
Hubert Leterme <hubert.leterme@unicaen.fr>
2025-08-28 08:42:55 +0200

    Bugfix in library initialization

--- wlmmuq/__init__.py
+++ wlmmuq/__init__.py
@@ -35,15 +35,25 @@ else:
 
 USE_PYCS = CONFIG_DATA.get('use_pycs', False)
 
-COSMOS_DIR = os.path.expanduser(CONFIG_DATA.get('cosmos_dir', None))
-KTNG_DIR = os.path.expanduser(CONFIG_DATA.get('ktng_dir', None))
+COSMOS_DIR = CONFIG_DATA.get('cosmos_dir', None)
+if COSMOS_DIR is not None:
+    COSMOS_DIR = os.path.expanduser(COSMOS_DIR)
+KTNG_DIR = CONFIG_DATA.get('ktng_dir', None)
+if KTNG_DIR is not None:
+    KTNG_DIR = os.path.expanduser(KTNG_DIR)
 
-PATH_TO_STD_NOISE = os.path.expanduser(CONFIG_DATA.get('path_to_std_noise', None))
-PATH_TO_MASK = os.path.expanduser(CONFIG_DATA.get('path_to_mask', None))
-PATH_TO_PS = os.path.expanduser(CONFIG_DATA.get('path_to_ps', None))
+PATH_TO_STD_NOISE = CONFIG_DATA.get('path_to_std_noise', None)
+if PATH_TO_STD_NOISE is not None:
+    PATH_TO_STD_NOISE = os.path.expanduser(PATH_TO_STD_NOISE)
+PATH_TO_MASK = CONFIG_DATA.get('path_to_mask', None)
+if PATH_TO_MASK is not None:
+    PATH_TO_MASK = os.path.expanduser(PATH_TO_MASK)
+PATH_TO_PS = CONFIG_DATA.get('path_to_ps', None)
+if PATH_TO_PS is not None:
+    PATH_TO_PS = os.path.expanduser(PATH_TO_PS)
 
-LEARNLETS_PRETRAINED_WEIGHTS_DIR = os.path.expanduser(
-    CONFIG_DATA.get('learnlets_pretrained_weights_dir', None)
-)
+LEARNLETS_PRETRAINED_WEIGHTS_DIR = CONFIG_DATA.get('learnlets_pretrained_weights_dir', None)
+if LEARNLETS_PRETRAINED_WEIGHTS_DIR is not None:
+    LEARNLETS_PRETRAINED_WEIGHTS_DIR = os.path.expanduser(LEARNLETS_PRETRAINED_WEIGHTS_DIR)
 
 KEY_REPLACEMENT_DICT = CONFIG_DATA.get("key_replacement_dict", None)
