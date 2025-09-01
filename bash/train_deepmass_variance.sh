77d82dae9dc6bdec2acf12dc3fe4489570c6c8a6
Hubert Leterme <hubert.leterme@unicaen.fr>
2025-09-01 10:24:16 +0200

    Update bash scripts

--- bash/train_deepmass_variance.sh
+++ bash/train_deepmass_variance.sh
@@ -7,19 +7,12 @@ checkpoint_dir=/ceph/chercheurs/leterme231/checkpoints/LP002_augmented_384
 # Check if correct number of arguments are provided
 if [ "$#" -lt 1 ]; then
   echo "Usage: $0 <GPU_ID> <NAME_DENOISER> [OPTION1 [OPTION 2 ...]]"
-  echo "Example: $0 0 deepmass_torch.UNetWienerInit_20250417_105243 [-a torch.UNetWienerInit] [-pred <filename> OR -o1 <timestamp>/ckp_19.pth.tar] [--loss mse]"
+  echo "Example: $0 0 deepmass_torch.UNetWienerInit_nepochs_20 -a torch.UNetWienerInit -t1 YYYYMMDD_hhmmss -e1 20"
   exit 1
 fi
 
 model_name=$2
 optional_args="${@:3}"
-
-# Update optional arguments with full path to saved order-1 model
-if [[ $optional_args == *"-o1 "* ]]; then
-  order1_model_filename=$(echo "$optional_args" | xargs | grep -oP '\-o1 \K[^\s]+' | xargs)
-  optional_args=$(echo "$optional_args" | sed "s|-o1 $order1_model_filename||g" | xargs)
-  optional_args="-o1 ${checkpoint_dir}/${model_name}/pe/${order1_model_filename} ${optional_args}"
-fi
 
 # Command to execute
 cmd=$(echo "python scripts/train.py ${path_to_augmented_dataset} --wiener-init --order2 ${optional_args} --lr-scheduler --checkpoint-dir ${checkpoint_dir}/${model_name} --cprofiler --cprofiler-max-nbatches 50 --cprofiler-wait 5 --cprofiler-cuda-synchronize --seed 42 -v" | xargs)
--- bash/train_denoiser_variance.sh
+++ bash/train_denoiser_variance.sh
@@ -7,19 +7,12 @@ checkpoint_dir=/ceph/chercheurs/leterme231/checkpoints/LP002_augmented_384
 # Check if correct number of arguments are provided
 if [ "$#" -lt 1 ]; then
   echo "Usage: $0 <GPU_ID> <NAME_DENOISER> [OPTION1 [OPTION 2 ...]]"
-  echo "Example: $0 0 denoiser_torch.DRUNet_small-model_scale_2.0e-1_scale-min_1.0e-1_20250417_105243 [-a torch.DRUNet] [-pred <filename> OR -o1 <timestamp>/ckp_19.pth.tar] [--scale 2.0e-1] [--scale-min 1.0e-1] [--loss mse]"
+  echo "Example: $0 0 denoiser_torch.SUNetNoiseAware_scale_0.2_scale-min_0.0_batch-size_16_nepochs_100_learning-rate_1e-3 -a torch.UNetWienerInit -t1 YYYYMMDD_hhmmss -e1 100 [--scale 2.0e-1] [--scale-min 1.0e-1]"
   exit 1
 fi
 
 name_denoiser=$2
 optional_args="${@:3}"
-
-# Update optional arguments with full path to saved order-1 model
-if [[ $optional_args == *"-o1 "* ]]; then
-  order1_model_filename=$(echo "$optional_args" | xargs | grep -oP '\-o1 \K[^\s]+' | xargs)
-  optional_args=$(echo "$optional_args" | sed "s|-o1 $order1_model_filename||g" | xargs)
-  optional_args="-o1 ${checkpoint_dir}/${name_denoiser}/pe/${order1_model_filename} ${optional_args}"
-fi
 
 # Command to execute
 cmd=$(echo "python scripts/train.py ${path_to_augmented_dataset} --denoiser --order2 ${optional_args} --lr-scheduler --checkpoint-dir ${checkpoint_dir}/${name_denoiser} --cprofiler --cprofiler-max-nbatches 50 --cprofiler-wait 5 --cprofiler-cuda-synchronize --seed 42 -v" | xargs)
