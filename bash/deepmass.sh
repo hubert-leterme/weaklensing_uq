47dce258e3f4b8751cae3f089214baffc028f693
Hubert Leterme <hubert.leterme@unicaen.fr>
2025-09-02 14:24:49 +0200

    Remove training underscores in bash scripts

--- bash/deepmass.sh
+++ bash/deepmass.sh
@@ -34,7 +34,7 @@ optional_args_cleaned=$(echo "$optional_args" \
   | sed 's/--//g' \
   | xargs \
   | sed 's/ /_/g')
-output_filename=$(echo "results_deepmass_${optional_args_cleaned}" | sed 's/__/_/g')
+output_filename=$(echo "results_deepmass_${optional_args_cleaned}" | sed 's/__/_/g' | sed 's/_\+$//')
 
 # Command to execute
 cmd=$(echo "python scripts/deepmass.py ${path_to_test_dataset} ${checkpoint_dir} ${optional_args} -o ${output_filename} --seed 42 -v" | xargs)
--- bash/pnpmass_calibration.sh
+++ bash/pnpmass_calibration.sh
@@ -42,7 +42,7 @@ optional_args_cleaned=$(echo "$optional_args" \
   | sed 's/--//g' \
   | xargs \
   | sed 's/ /_/g')
-output_filename=$(echo "cqr_pnpmass_${optional_args_cleaned}" | sed 's/__/_/g')
+output_filename=$(echo "cqr_pnpmass_${optional_args_cleaned}" | sed 's/__/_/g' | sed 's/_\+$//')
 
 # Command to execute
 cmd=$(echo "python scripts/pnpmass_calibration.py ${path_to_calibration_dataset} ${checkpoint_dir} ${optional_args} -o ${output_filename} --seed 42 -v" | xargs)
