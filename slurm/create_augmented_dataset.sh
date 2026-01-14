6fd26b780adf262a2e8994933141123766eebaa5
Hubert Leterme (from Feynman) <hubert.leterme@cea.fr>
2026-01-14 13:56:47 +0100

    Update slurm bash script for creating augmented dataset

--- slurm/create_augmented_dataset.sh
+++ slurm/create_augmented_dataset.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #SBATCH --mail-user hubert.leterme@cea.fr
-#SBATCH --job-name create_augmented_dataset
-#SBATCH --time 04:00:00
-#SBATCH --cpus-per-task 4
-#SBATCH --output slurm/out/output.log
-#SBATCH --error slurm/out/error.log
+#SBATCH --job-name=create_augmented_dataset
+#SBATCH --partition=htc
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=50GB
+#SBATCH --output slurm/out/out_%j.log
 
-python scripts/create_augmented_dataset.py --angle-step 1 --niter-per-angle 2 --seed 42 -v
+python scripts/create_augmented_dataset.py --zbins 0.4 0.8 1.2 1.6 2.0 -b 10 --angle-batch-size 8 --angle-step 1 --niter-per-angle 2 -w 8 --seed 42 -v
