4198ebf8c16833ae80ae496d0ac3ea6bb4b562e1
Hubert Leterme (from Feynman) <hubert.leterme@cea.fr>
2026-01-23 11:02:18 +0100

    Update slurm bash script

--- slurm/train_denoiser_zbins.sh
+++ slurm/train_denoiser_zbins.sh
@@ -4,7 +4,9 @@
 #SBATCH --partition=gpu
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:4g.20gb:1
+#SBATCH --gres=gpu:2g.10gb
+#SBATCH --time=24:00:00
+#SBATCH --mem=50GB
 #SBATCH --output slurm/out/out_%j.log
 
 python scripts/train.py -a SUNetNoiseAware -d --scale 0.3 --scale-min 0.0 -b 16 -e 100 -lr 1e-3 --lr-scheduler -c denoiser_arch_SUNetNoiseAware_zbins_scale_0.3_scale-min_0.0_batch-size_16_nepochs_100_learning-rate_1e-3 -w 8 --seed 42 -v
