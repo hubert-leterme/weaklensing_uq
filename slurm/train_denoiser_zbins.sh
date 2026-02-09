0cbb8446842f9a01a09f5c8181e61b4b4020effa
hubert leterme <hl285110@dappcr253.extra.cea.fr>
2026-02-09 13:30:24 +0100

    Update slurm bash script

--- slurm/train_denoiser_zbins.sh
+++ slurm/train_denoiser_zbins.sh
@@ -8,5 +8,5 @@
 #SBATCH --mem=50GB
 #SBATCH --output ~/work/Log/slurm_out/out_%j.log
 
-python scripts/train.py -a SUNetNoiseAware -d --scale 0.3 --scale-min 0.0 -b 16 -e 100 -lr 1e-3 --lr-scheduler -c denoiser_arch_SUNetNoiseAware_zbins_scale_0.3_scale-min_0.0_batch-size_16_nepochs_100_learning-rate_1e-3 -w 8 --seed 42 -v
+python scripts/train.py -a SUNetNoiseAware -d --scale 0.1 --scale-min 0.0 -b 16 -e 100 -lr 1e-3 --lr-scheduler -c denoiser_arch_SUNetNoiseAware_zbins_scale_0.3_scale-min_0.0_batch-size_16_nepochs_100_learning-rate_1e-3 -w 8 --seed 42 -v
 # python scripts/train.py -a SUNetNoiseAware -d --scale 0.1 --scale-min 0.0 -b 16 -e 100 -lr 1e-3 --lr-scheduler -c denoiser_arch_SUNetNoiseAware_zbins_scale_0.1_scale-min_0.0_batch-size_16_nepochs_100_learning-rate_1e-3 -r -tr 20260203_134451 -er 83 -w 8 --seed 42 -v
